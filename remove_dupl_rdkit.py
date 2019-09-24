b77a784441c31213f90b756595bcf1b79c8ee138
Pavel Polishchuk <pavel_polishchuk@ukr.net>
2019-09-24 11:30:08 +0200

    Add support of more input file formats in remove_dupl_rdkit.py

--- remove_dupl_rdkit.py
+++ remove_dupl_rdkit.py
@@ -5,6 +5,7 @@ __author__ = 'Pavel Polishchuk'
 import argparse
 from rdkit import Chem
 from rdkit.Chem import inchi
+from read_input import read_input
 
 
 def get_inchi_key(mol, stereo):
@@ -19,24 +20,18 @@ def main_params(in_fname, dupl_fname, out_fname, ref_fname, stereo):
 
     saver = Chem.SDWriter(out_fname)
 
-    d = dict()  # key - smiles, value - list of names
+    d = dict()  # key - inchi_key, value - list of names
 
     # read reference compounds
     if ref_fname is not None:
-        for i, mol in enumerate(Chem.SDMolSupplier(ref_fname)):
+        for i, (mol, mol_name) in enumerate(read_input(ref_fname)):
             if mol is not None:
-                mol_name = mol.GetProp("_Name")
-                if not mol_name:
-                    mol_name = 'autoname_id_' + str(i)
                 inchi_key = get_inchi_key(mol, stereo)
                 if inchi_key not in d.keys():
                     d[inchi_key] = [mol_name]
 
-    for i, mol in enumerate(Chem.SDMolSupplier(in_fname)):
+    for i, (mol, mol_name)in enumerate(read_input(in_fname)):
         if mol is not None:
-            mol_name = mol.GetProp("_Name")
-            if not mol_name:
-                mol_name = 'autoname_id_' + str(i)
             inchi_key = get_inchi_key(mol, stereo)
             if inchi_key not in d.keys():
                 saver.write(mol)
@@ -55,15 +50,16 @@ def main():
 
     parser = argparse.ArgumentParser(description='Remove duplicates by inchi keys comparison.')
     parser.add_argument('-i', '--input', metavar='input.sdf', required=True,
-                        help='input sdf file.')
+                        help='input SDF, SMI, SDF.GZ or PKL file.')
     parser.add_argument('-r', '--reference', metavar='reference.sdf', required=False, default=None,
                         help='reference sdf file. If this file is specified than compounds from the input file '
                              'will be omitted if they are present in the reference file. If the input file contains '
-                             'duplicated structures which are not in the reference file they will be also filtered.')
+                             'duplicated structures which are not in the reference file they will be also filtered. '
+                             'Supported file formats are SDF, SMI, SDF.GZ or PKL.')
     parser.add_argument('-d', '--duplicates', metavar='duplicates.txt', required=False, default=None,
                         help='if specified names of removed duplicates will be stored in this file.')
     parser.add_argument('-o', '--output', metavar='output.sdf', required=True,
-                        help='output sdf file with removed duplicates.')
+                        help='output SDF file with removed duplicates.')
     parser.add_argument('-s', '--stereo', action='store_true', default=False,
                         help='if set this flag stereoconfiguration will be considered during duplicate searching. '
                              'If set then isotopes will be considered as well.')

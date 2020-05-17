cabcbb73c517c05f8f5d6d1b8c1dcf7e1a46e4dd
Alina <alina.kutlushina@pharminnotech.com>
2020-05-17 10:00:39 +0200

    added a description

--- convert_to_pdbfile.py
+++ convert_to_pdbfile.py
@@ -48,12 +48,13 @@ def convert_to_pdb(input, output_path, nprocess):
 
 
 def entry_point():
-    parser = argparse.ArgumentParser(description='Convert to a PDB file with the addition of hydrogens and '
-                                                 'coordinates, if the initial structure has a 2d format',
+    parser = argparse.ArgumentParser(description='Convert input molecules to individual PDB files. '
+                                                 'Hydrogens will be added and a random conformer will be generated '
+                                                 'if the molecule does not have 3D coordinates.',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-i', '--input', type=str, required=True,
+    parser.add_argument('-i', '--input', metavar='FILENAME', type=str, required=True,
                         help='input path to .smi or .sdf file')
-    parser.add_argument('-o', '--output_path', type=str, required=True,
+    parser.add_argument('-o', '--output_path', metavar='DIRNAME', type=str, required=True,
                         help='path to a folder where will store molecules in pdb format files')
     parser.add_argument('-c', '--ncpu', type=int, default=1,
                         help='number of cpu to use for calculation.')

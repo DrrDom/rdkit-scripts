4506d74f889deb303f2df004f9e11de939618f91
guzelka <minibaeva07501@gmail.com>
2022-05-26 14:14:42 +0200

    added checking a molecule in 3D before assigning stereoconfiguration from 3D

--- sdf2smi.py
+++ sdf2smi.py
@@ -10,7 +10,8 @@ from rdkit import Chem
 def calc(input_fname, output_fname, field_name, extract_fields, sep):
     with open(output_fname, "wt") as f:
         for m in Chem.SDMolSupplier(input_fname):
-            Chem.AssignStereochemistryFrom3D(m)
+            if m.GetConformer().Is3D():
+                Chem.AssignStereochemistryFrom3D(m)
             if m:
                 smi = Chem.MolToSmiles(m, isomericSmiles=True)
                 if field_name:

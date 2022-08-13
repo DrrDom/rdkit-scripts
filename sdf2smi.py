12b2d04db0c865144abc2dbf7b064dc37d1ff807
Pavel Polishchuk <pavel_polishchuk@ukr.net>
2022-08-13 17:27:19 +0200

    Add header to output file if fields are extracted in sdf2smi.py

--- sdf2smi.py
+++ sdf2smi.py
@@ -9,6 +9,8 @@ from rdkit import Chem
 
 def calc(input_fname, output_fname, field_name, extract_fields, sep):
     with open(output_fname, "wt") as f:
+        if extract_fields is not None:
+            f.write('smi\tid\t' + '\t'.join(extract_fields) + '\n')
         for m in Chem.SDMolSupplier(input_fname):
             if m.GetConformer().Is3D():
                 Chem.AssignStereochemistryFrom3D(m)

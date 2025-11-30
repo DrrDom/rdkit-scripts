ac93ed8a17ad6c15fd7851bc8dcad57c0a8671c2
Pavel Polishchuk <pavel_polishchuk@ukr.net>
2025-11-30 14:56:26 +0100

    Add get_map.py script which returns UMAP coordinates for input structures

---
+++ get_map.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+import argparse
+from rdkit import Chem
+from rdkit.Chem import AllChem, rdMolDescriptors
+import numpy as np
+import umap
+from read_input import read_input
+
+
+def compute_fps(mols, fp_type="morgan", nBits=2048, radius=2):
+    fps = []
+    for mol in mols:
+        if mol is None:
+            fps.append(np.zeros((nBits,), dtype=int))
+            continue
+        if fp_type == "morgan":
+            fp = AllChem.GetMorganFingerprintAsBitVect(mol, radius, nBits=nBits)
+        elif fp_type == "atom_pair":
+            fp = rdMolDescriptors.GetHashedAtomPairFingerprintAsBitVect(mol, nBits=nBits)
+        elif fp_type == "torsion":
+            fp = rdMolDescriptors.GetHashedTopologicalTorsionFingerprintAsBitVect(mol, nBits=nBits)
+        elif fp_type == "rdkit":
+            fp = Chem.RDKFingerprint(mol, fpSize=nBits)
+        else:
+            raise ValueError(f"Unsupported fingerprint type: {fp_type}")
+        arr = np.zeros((1,), dtype=int)
+        Chem.DataStructs.ConvertToNumpyArray(fp, arr)
+        fps.append(arr)
+    return np.array(fps)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="2D UMAP projection of molecules")
+    parser.add_argument("-i", "--input", required=True, help="Input SMILES file")
+    parser.add_argument("-o", "--output", required=True, help="Output coordinates file")
+    parser.add_argument("-f", "--fingerprint", choices=["morgan", "atom_pair", "torsion", "rdkit"],
+                        default="morgan", help="Type of fingerprint to compute")
+    parser.add_argument("--radius", type=int, default=2, help="Radius for Morgan fingerprints")
+    parser.add_argument("--n_bits", type=int, default=2048, help="Number of bits in fingerprint")
+    parser.add_argument("--n_neighbors", type=int, default=15, help="UMAP n_neighbors parameter")
+    parser.add_argument("--min_dist", type=float, default=0.1, help="UMAP min_dist parameter")
+    parser.add_argument("--metric", type=str, default="jaccard", help="UMAP distance metric")
+    args = parser.parse_args()
+
+    fps = compute_fps([m for m, mol_name in read_input(args.input)], args.fingerprint, args.n_bits, args.radius)
+
+    reducer = umap.UMAP(
+        n_neighbors=args.n_neighbors,
+        min_dist=args.min_dist,
+        metric=args.metric,
+        n_components=2
+    )
+    coords = reducer.fit_transform(fps)
+
+    with open(args.input) as f:
+        with open(args.output, "w") as out:
+            for line, (x, y) in zip(f, coords):
+                tmp = line.strip().split()[:2]
+                x = str(round(x, 4))
+                y = str(round(y, 4))
+                out.write("\t".join(tmp) + '\t' + x + '\t' + y + '\n')
+
+
+if __name__ == "__main__":
+    main()
